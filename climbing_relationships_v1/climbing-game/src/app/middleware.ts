35fe60827b56a917f0a567caff21e0f5bb49cde0
Ethan-Orion-Reese <ethanorionreese@gmail.com>
2025-09-21 16:45:31 -0400

    fixing ESLint error for using "any"

--- climbing_relationships_v1/climbing-game/src/app/middleware.ts
+++ climbing_relationships_v1/climbing-game/src/app/middleware.ts
@@ -1,16 +1,31 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { Socket } from 'socket.io';
+import { Socket, Server as SocketIOServer } from 'socket.io';
+
+// Define the server type to avoid using 'any'
+interface CustomSocket {
+  server: {
+    io?: Socket;
+    [key: string]: unknown;
+  };
+}
+
+// Extend the NextRequest type to include our custom socket
+interface CustomNextRequest extends NextRequest {
+  socket: CustomSocket;
+}
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const customRequest = request as CustomNextRequest;
+  
   // Add socket.io server to the request object if it doesn't exist
-  if (!request.socket.server) {
-    request.socket.server = {} as any;
+  if (!customRequest.socket.server) {
+    customRequest.socket.server = {};
   }
   
-  if (!request.socket.server.io) {
-    request.socket.server.io = null as unknown as Socket;
+  if (!customRequest.socket.server.io) {
+    customRequest.socket.server.io = null as unknown as Socket;
   }
   
   return NextResponse.next();
